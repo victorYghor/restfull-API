ba473f057f785e9adff14d2fd528e11f282a0972
victor <devictoryghor@gmail.com>
2023-11-14 20:50:03 -0300

    fix the testes

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -3,11 +3,12 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = DemoApplicationTests.class)
 class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
+
 	}
 
 }
